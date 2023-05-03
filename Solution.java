d57b773278026106a7225722f20cd44529c13cbc
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-05-03 15:24:31 +0100

    done

--- Solution.java
+++ Solution.java
@@ -3,15 +3,16 @@ import java.util.Arrays;
 public class Solution {
     public int findPeakElement(int[] nums) {
         int elementIndex = 0;
-        int[] numsCopy ;
-        numsCopy = nums;
+        int[] numsCopy =Arrays.copyOf(nums, nums.length);;
         Arrays.sort(numsCopy);
+        int max = numsCopy[numsCopy.length - 1];
 
 
         //get the index of the final number in the sorted array in the nonsorted array
         for (int i = 0; i < numsCopy.length; i++) {
-            if (numsCopy[numsCopy.length - 1] == nums[i]) {
+            if (max == nums[i]) {
                 elementIndex = i;
+                break;
             }
         }
 
